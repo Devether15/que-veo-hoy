514e28999a0f6c7abd7936a065f5c75cd046c706
Devether15 <josemomoya.inf@gmail.com>
2018-12-16 19:46:09 -0300

    agregando limit, order by y paginado en el callback de la funcion obtenerTodas

--- servidor/controladores/controlador.js
+++ servidor/controladores/controlador.js
@@ -3,30 +3,45 @@ const con = require('../lib/conexionbd');
 function obtenerTodas ( req, res ) {
     
     let sql = `SELECT * FROM pelicula INNER JOIN genero ON pelicula.genero_id = genero.id `;
+
+    let limite = ' LIMIT ' + (req.query.pagina - 1) * 20 + ',' + req.query.cantidad;
+
+    let contador = 'SELECT COUNT(*) AS total FROM pelicula';
                        
     
     if( req.query.titulo ) { 
-    sql += `AND titulo LIKE "%${req.query.titulo}%" `;
+    sql += `AND titulo LIKE "%${req.query.titulo}%" ORDER BY ${req.query.columna_orden} ${req.query.tipo_orden}`;
     } if( req.query.genero ) {
-        sql += `AND genero.id = ${req.query.genero}`;
+        sql += `AND genero.id = ${req.query.genero} ORDER BY ${req.query.columna_orden} ${req.query.tipo_orden}`;
     } if( req.query.anio ) {
-        sql += `AND anio = ${req.query.anio}`;
+        sql += `AND anio = ${req.query.anio} ORDER BY ${req.query.columna_orden} ${req.query.tipo_orden}`;
     } if ( req.query.orden ) {
-        sql += `AND orden = ${req.query.orden}`
+        sql += `AND orden = ${req.query.orden} ORDER BY ${req.query.columna_orden} ${req.query.tipo_orden}`;
     }
 
-    con.query( sql, function(error, resultado, fields) {
+    con.query( sql + limite, function(error, resultado, fields) {
         if (error) {
         console.log("Hubo un error", error.message);
         return res.status(404).send("Hubo un error en la consulta");   
-    } 
-    let response = {
-        'peliculas':resultado
+    };
+    
+    con.query(contador, function (error, resultadoTotal, fields){
+        if(error){
+            console.log("Hubo un error", error.message);
+            return res.status(404).send("Hubo un error en la consulta");
+        };
+    
+        let response = {
+        'peliculas':resultado,
+        'resultadoTotal':resultadoTotal[0].resultadoTotal,
+
     };
 
     res.send(JSON.stringify(response));
 
     });
+});
+
 }
 
 function obtenerGeneros ( req, res ){
